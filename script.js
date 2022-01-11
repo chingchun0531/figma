c2e9ac6bfdf31b7a661a53aa4ba592d5809c26aa
chingchunlin890531 <chingchunlin890531@gmail.com>
2022-01-11 18:24:22 +0800

    0111

--- script.js
+++ script.js
@@ -1,2 +1,26 @@
 const d = new Date();
 document.getElementById('date').innerHTML = d;
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlide(n){
+    showSlides(slideIndex += n);
+}
+
+function currentSlide(n){
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n){
+    var i;
+    var slides = document.getElementsByClassName("myslides");
+    var dots = document.getElementsByClassName("dot");
+    if(n>slides.length){
+        slideIndex = 1
+    }
+    if(n<1){
+         slideIndex = slides.length
+    }
+    
+}
